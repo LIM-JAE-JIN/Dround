5ee171422e7679f0b1f28dcf05c5589fe4423710
JAEJIN <wowls9416@naver.com>
2023-06-04 22:59:18 +0900

    test

--- js/common.js
+++ js/common.js
@@ -1,3 +1,22 @@
 $(document).ready( function(){
-    
+
+
+    // 회원가입 동의 체크 기능
+    $('#allAgreeCheck').on('change',function() {
+        if ($('#allAgreeCheck').is(":checked")) {
+            $('input[name="subCheck"], #notifyAgreeCheck').prop('checked',true);
+        } else{
+            $('input[name="subCheck"], #notifyAgreeCheck').prop('checked',false);
+        }
+        // checkValidata();
+    });
+    $('input[name="subCheck"], input[name="notifyAgreeCheck"]').on('change',function() {
+        const subCheck = $('input[name="subCheck"]:checked').length;
+        const notifyAgreeCheck = $('input[name="notifyAgreeCheck"]').is(":checked");
+        if( subCheck === 3 && notifyAgreeCheck === true ){
+            $('#allAgreeCheck').prop('checked',true);
+        }else{
+            $('#allAgreeCheck').prop('checked',false);
+        }
+    });
 })
