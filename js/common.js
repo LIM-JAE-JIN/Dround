19f1faabf09aae2e76fa8f4353ce568ac2679c7e
sonwooheon <b29299461@gmail.com>
2023-09-18 19:46:20 +0900

    ios 댓글쪽 테스트

--- js/common.js
+++ js/common.js
@@ -54,6 +54,12 @@ $(document).ready( function(){
     });
     $(window).scroll();
 
+    if (/iPhone|iPad|iPod/i.test(navigator.userAgent)) {
+        // iOS 기기인 경우에만 필요한 스타일을 적용
+        $('.dr-details-comment .dr-textarea-wrap textarea::placeholder').css({'position':'absolute','top':'50%','transform':'translateY(-50%)','color':'#383A52'});
+        {position: absolute; top: 50%; transform: translateY(-50%); color: #383A52;}
+    }
+
 })
 //팝업 열기
 function drPopupOpen(popName){
